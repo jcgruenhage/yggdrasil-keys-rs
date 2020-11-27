214d038c14d1a035cce06eb3d7b82237e7c5e6bc
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2020-11-27 01:25:28 +0100

    feat: add first benchmark

---
+++ benches/key_generation.rs
@@ -0,0 +1,18 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rand::thread_rng;
+use std::net::Ipv6Addr;
+use yggdrasil_keys::NodeIdentity;
+
+fn generate_keys(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("generate_key", |b| {
+        b.iter(|| {
+            let node = NodeIdentity::new(&mut rng);
+            let leading_ones = node.node_id().strength();
+            black_box(leading_ones);
+        })
+    });
+}
+
+criterion_group!(benches, generate_keys);
+criterion_main!(benches);
